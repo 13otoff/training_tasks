d36807f3df88f3866cc3731e3cf8636b4feb9b36
13otoff <sergio.zotov9@gmail.com>
2022-01-18 13:54:04 +0400

    Added all tasks_1.2

---
+++ Task 1/Task_1.2/Task_1.2.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task_1._2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string:");
+            string first = Console.ReadLine();
+
+            Console.WriteLine("Enter second string:");
+            string second = Console.ReadLine();
+
+            char[] charsOfFirst = first.ToCharArray();
+            char[] charsOfSecond = second.ToCharArray();
+            string result = "";
+
+
+            for (int i = 0; i < charsOfFirst.Length; i++)
+            {
+                result += charsOfFirst[i];
+                for (int j = 0; j < charsOfSecond.Length; j++)
+                {
+                    if (charsOfSecond[j] == charsOfFirst[i])
+                    {
+                        result += charsOfFirst[i];
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("Result:");
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Task 1/Task_1.2/Task_1.2.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task_1._2._3
+{
+    
+    // Антон хорошо начал утро: послушал Стинга, выпил кофе и посмотрл Звездные Войны
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string:");
+            string str = Console.ReadLine();
+
+            int countOfLowWords = 0;
+
+            string[] words = str.Split(new char[] { ' ', ',', '.', ':', ';', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (char.IsLower(words[i][0]))
+                    countOfLowWords++;
+            }
+            Console.WriteLine("Count of words writing with lowercase letter: {0}", countOfLowWords);
+        }
+    }
+}
