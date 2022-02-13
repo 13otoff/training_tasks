34bc585e4134f0a7870cfaffbdb3176b72553f96
13otoff <sergio.zotov9@gmail.com>
2022-02-13 20:15:23 +0400

    Added tasks_3.1

---
+++ Task 3/Task_3.1/Task_3.1.1/Person.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3._1._1
+{
+    public class Person
+    {
+        private int i;
+        private int countOfPeople;
+        private int number;
+
+        public int Number
+        {
+            get { return number; }
+            private set 
+            {
+                if (value > 1 && value <= countOfPeople)
+                    number = value;
+                else
+                    throw new ArgumentException("Number must be less than count of people");
+            }
+        }
+
+
+        public int CountOfPeople
+        {
+            get { return countOfPeople; }
+            private set 
+            {
+                if (value > 1 && value >= number)
+                    countOfPeople = value;
+                else
+                    throw new ArgumentException("Circle of people must consist of more than 1 person");
+            }
+        }
+
+        public Person(int countOfPeople, int number, int i = 1)
+        {
+            CountOfPeople = countOfPeople;
+            Number = number;
+            this.i = i;
+        }
+
+        public override string ToString()
+        {
+            string result = $"Round {i}. Deleted person. People left: {CountOfPeople - 1}";
+
+            if (Number < CountOfPeople)
+                return result;
+            else
+                return result + $"{Environment.NewLine}Game over. Cannot be cross out person";
+        }
+
+
+
+        public void Deletion()
+        {
+            Console.WriteLine(ToString());
+            if (Number < CountOfPeople)
+            {
+                i++;
+                CountOfPeople--;
+                Deletion();
+            }
+            return;
+        }
+    }
+}
---
+++ Task 3/Task_3.1/Task_3.1.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_3._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter N");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter what number of people will be crossed out");
+            int number = int.Parse(Console.ReadLine());
+
+            Person person = new Person(n, number);
+            Console.WriteLine("Generated a circle of people. Start to delete every second person");
+            person.Deletion();
+        }
+    }
+}
